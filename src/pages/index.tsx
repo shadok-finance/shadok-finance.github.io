4e3e2385b2d48b0461baffe341b56fe8190ddc9f
shadok-gabu <gabu@shadok.finance>
2021-12-08 22:23:01 +0100

    Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import { injectStyle } from "react-toastify/dist/inject-style";
+import { Helmet } from "react-helmet";
 import { Layout, Footer } from "@/uikit/components";
 import { Header, MenuElement } from "@/sections/Header";
 import { Hero, About, Roadmap, Faq, Community } from "@/sections";
 import { Web3Provider } from "@/web3";
 import { Web3ContextWrapper } from "@/web3/Web3Context";
 import { UserTokenAccountContextWrapper } from "@/web3/UserTokenAccountContext";
+import favicon from "@/assets/icons/c999_favicon.svg";
 
 const menuElements: MenuElement[] = [
   { label: "About", url: "#about" },
@@ -24,6 +26,11 @@ if (typeof window !== "undefined") {
 export default function Home() {
   return (
     <main>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Shadoks</title>
+        <link id="favicon" rel="icon" href={favicon} />
+      </Helmet>
       <Layout>
         <Web3Provider>
           <Web3ContextWrapper>
