8d934a43ea90048b3664164a607902c076845f7e
shadok-gabu <gabu@shadok.finance>
2021-12-10 18:00:20 +0100

    Display supplies without a connected wallet

--- src/web3/useTotalGulpSupply.ts
+++ src/web3/useTotalGulpSupply.ts
@@ -1,29 +1,25 @@
 import React from "react";
 import { Maybe } from "true-myth";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { Web3Context } from "@/web3/Web3Context";
+import { useConnection } from "@solana/wallet-adapter-react";
 import { GULP_HOLE } from "@/web3/common";
 
 export const useTotalGulpSupply = (): [Maybe<number>, () => void] => {
   const [totalSupply, setTotalSupply] = React.useState<Maybe<number>>(
     Maybe.nothing(),
   );
-  const web3 = React.useContext(Web3Context);
+  const { connection } = useConnection();
 
-  const getSolInGulp = (): Promise<Maybe<number>> =>
-    web3.match({
-      Ok: async (provider) => {
-        try {
-          const solBalance = await provider.connection.getBalance(GULP_HOLE);
+  const getSolInGulp = async (): Promise<Maybe<number>> => {
+    try {
+      const solBalance = await connection.getBalance(GULP_HOLE);
 
-          return Maybe.of(solBalance / LAMPORTS_PER_SOL);
-        } catch (err) {
-          console.error("Failed to get total amount of $SOL in the Gulp");
-          return Maybe.nothing<number>();
-        }
-      },
-      Err: () => Promise.resolve(Maybe.nothing<number>()),
-    });
+      return Maybe.of(solBalance / LAMPORTS_PER_SOL);
+    } catch (err) {
+      console.error("Failed to get total amount of $SOL in the Gulp");
+      return Maybe.nothing<number>();
+    }
+  };
 
   const updateTotalSupply = async () => {
     const supply = await getSolInGulp();
@@ -32,7 +28,7 @@ export const useTotalGulpSupply = (): [Maybe<number>, () => void] => {
 
   React.useEffect(() => {
     updateTotalSupply();
-  }, [web3]);
+  }, [connection]);
 
   return [totalSupply, updateTotalSupply];
 };
--- src/web3/useTotalMemecoinSupply.ts
+++ src/web3/useTotalMemecoinSupply.ts
@@ -1,30 +1,24 @@
 import React from "react";
 import { Maybe } from "true-myth";
-import { Web3Context } from "@/web3/Web3Context";
+import { useConnection } from "@solana/wallet-adapter-react";
 import { TOKEN_MINT_ACCOUNT } from "@/web3/common";
 
 export const useTotalMemecoinSupply = (): [Maybe<number>, () => void] => {
   const [totalSupply, setTotalSupply] = React.useState<Maybe<number>>(
     Maybe.nothing(),
   );
-  const web3 = React.useContext(Web3Context);
+  const { connection } = useConnection();
 
-  const getTotalMemecoinSupply = (): Promise<Maybe<number>> =>
-    web3.match({
-      Ok: async (provider) => {
-        try {
-          const response = await provider.connection.getTokenSupply(
-            TOKEN_MINT_ACCOUNT,
-          );
+  const getTotalMemecoinSupply = async (): Promise<Maybe<number>> => {
+    try {
+      const response = await connection.getTokenSupply(TOKEN_MINT_ACCOUNT);
 
-          return Maybe.of(response.value.uiAmount);
-        } catch (err) {
-          console.error("Failed to get total C999 supply");
-          return Maybe.nothing<number>();
-        }
-      },
-      Err: () => Promise.resolve(Maybe.nothing<number>()),
-    });
+      return Maybe.of(response.value.uiAmount);
+    } catch (err) {
+      console.error("Failed to get total C999 supply");
+      return Maybe.nothing<number>();
+    }
+  };
 
   const updateTotalSupply = async () => {
     const supply = await getTotalMemecoinSupply();
@@ -33,7 +27,7 @@ export const useTotalMemecoinSupply = (): [Maybe<number>, () => void] => {
 
   React.useEffect(() => {
     updateTotalSupply();
-  }, [web3]);
+  }, [connection]);
 
   return [totalSupply, updateTotalSupply];
 };
